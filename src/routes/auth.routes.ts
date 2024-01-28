77f149d4d5abc9f4757de6528a5c471e57826a06
DESKTOP-7V3NUJB\Danilo-PC <rapaic.danilo9@gmail.com>
2024-01-28 16:26:51 +0100

    add change password method

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 const authRouter = require("express").Router();
 import verifySignUp from '../middleware/auth.middleware'
-import { signup, signin } from '../controllers/auth.controller';
+import { signup, signin, changePassword } from '../controllers/auth.controller';
 
-authRouter.post('/signup',[
-    verifySignUp.checkDuplicateEmail,
-    verifySignUp.checkRolesExisted
-  ], signup)
-  authRouter.post('/signin', signin)
-//   authRouter.post('/change_password', changePassword)
+authRouter.post('/signup', [
+  verifySignUp.checkDuplicateEmail,
+  verifySignUp.checkRolesExisted
+], signup)
+authRouter.post('/signin', signin)
+authRouter.post('/change_password', verifySignUp.verifyToken, changePassword)
 
 export default authRouter;
